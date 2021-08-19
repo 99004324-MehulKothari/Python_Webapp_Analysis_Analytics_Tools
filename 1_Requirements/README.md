0038e67a010fd29f1a7c3d617ecf230822901b4f
MEHULK007 <mehul.kothari@ltts.com>
2021-08-19 16:12:52 +0530

    updates

---
+++ 1_Requirements/README.md
@@ -0,0 +1,33 @@
+## High Level Requirements: 
+| ID | Description | Category | 
+| ----- | ----- | ----- |
+| HR1 | Create a web app for analysis/analytics | Basic |
+| HR2 | Get user choice of 2 companies to analyse | Technical |
+| HR3 | Get user input of company and year to predict | Technical |
+| HR4 | Provide visual insight based on user data | Technical |
+| HR5 | Provide a predicted value based on user data | Technical |
+| HR6 | Deploy and host the web app on Microsoft Azure Cloud | Technical |
+| HR7 | Make use of different azure services to enhance the application | Technical |
+| HR8 | Make use of different azure services for learning purpose | Technical |
+
+## Low Level Requirements: 
+| ID | Description | HLR ID | 
+| ----- | ----- | ----- | 
+| LR1 | Build index page for user selecion of analysis/analytics  | HR1 |
+| LR2 | Use Html forms to get user inputs | HR1 |
+| LR3 | Use pygal libraray for providing visual insights | HR4 |
+| LR4 | Use ML algorithm for doing the prediction task for analytics tool  | HR5 |
+| LR5 | Host the web app using azure app service  | HR6 |
+| LR6 | Use Sql Database to store the tables  | HR7 |
+| LR7 | Use storage to have all other services linked by storing outputs of those services in it | HR7 |
+| LR8 | Use Monitor service to monitor the performance and other key metrics of web app  | HR7 |
+| LR9 | Use Azure AD to provide SSL Authentication to the webapp | HR8 |
+| LR10 | Use CDN and Front Door to provide more secure and low latency webapp  | HR7 |
+| LR11 | Use ML service to develop efficient ML models  | HR8 |
+| LR12 | Use Event Hub for real time data ingestion | HR8 |
+| LR13 | Use WAF to set firewall rules for application  | HR7 |
+| LR14 | Use Managed Identity for managing the developer credentials | HR8 |
+| LR15 | Use Devops starter to create a CI/CD pipeline for webapp | HR8 |
+| LR16 | Use Azure Alerts to detect anamolies in web app based on our set thresholds | HR7 |
+| LR17 | Use Data Share to transfer data among different servers | HR7 |
+
--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+This program is to develop a simple web application
+"""
 from logging import debug
 from flask import Flask, app,redirect,url_for,render_template,request
 import pyodbc
@@ -7,157 +10,144 @@ from sklearn.linear_model import LinearRegression
 import pygal
 from pygal.style import Style
 
-server = 'sql-car.database.windows.net'
-database = 'car_details'
-username = 'azure-car'
-password = '{India#123}'   
-driver= '{ODBC Driver 17 for SQL Server}'
+SERVER = 'sql-car.database.windows.net'
+DATABASE = 'car_details'
+USERNAME = 'azure-car'
+PASSWORD = '{India#123}'
+DRIVER= '{ODBC Driver 17 for SQL Server}'
 app=Flask(__name__)
-analysis_drop1=0
-analysis_drop2=0
-analytics_drop=0
-analytics_year=0
+ANALYSIS_DROP1=0
+ANALYSIS_DROP2=0
+ANALYTICS_DROP=0
+ANALYTICS_YEAR=0
 
 
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
+    """
+    This function is used to create the index page of our app with 2 buttons analysis and analytics
+    """
     if request.method == 'POST':
         if request.form['action1'] == 'Analysis':
             return redirect(url_for('analysis'))
-        elif  request.form['action1'] == 'Analytics':
-            return redirect(url_for('analytics'))
         else:
-            pass # unknown
-    elif request.method == 'GET':
-        return render_template('index.html')
-    
+            return redirect(url_for('analytics'))
     return render_template('index.html')
 
 @app.route('/analysis',methods=['GET', 'POST'])
 def analysis():
-
+    """
+    This function is used to create an analysis page with
+    3 buttons ,2 for selecting the company and a submit button
+    """
     if request.method == 'POST':
-        if request.form['action4'] == 'Submit' and request.form['action2'] != "Choose..." and request.form['action3'] != "Choose...":
-            global analysis_drop1,analysis_drop2
-            analysis_drop1=request.form['action2']
-            analysis_drop2=request.form['action3']
+        if request.form['action4'] == 'Submit' and \
+        request.form['action2'] != "Choose..." and \
+        request.form['action3'] != "Choose...":
+            global ANALYSIS_DROP1,ANALYSIS_DROP2
+            ANALYSIS_DROP1=request.form['action2']
+            ANALYSIS_DROP2=request.form['action3']
             return redirect(url_for('analysis_results'))
         else:
             pass # unknown
-    elif request.method == 'GET':
-        return render_template('analysis.html')
-    
     return render_template('analysis.html')
 
 @app.route('/analytics',methods=['GET', 'POST'])
 def analytics():
-
+    """
+    This function is used to create an analysis page with 3 buttons
+    - selecting the company,enter a year and a submit button
+    """
     if request.method == 'POST':
-        if request.form['action7'] == 'Submit' and request.form['action5'] != "Choose..." and request.form['action6'] !=None and request.form['action6'].isdigit() and int(request.form['action6'])>=2000 and int(request.form['action6'])<=2100 :
-            global analytics_drop,analytics_year 
-            analytics_drop= request.form['action5']
-            analytics_year=request.form['action6']
+        if request.form['action7'] == 'Submit' and \
+            request.form['action5'] != "Choose..." and \
+            request.form['action6'] !=None and \
+            request.form['action6'].isdigit() and \
+            int(request.form['action6'])>=2000 and \
+            int(request.form['action6'])<=2100 :
+            global ANALYTICS_DROP,ANALYTICS_YEAR
+            ANALYTICS_DROP= request.form['action5']
+            ANALYTICS_YEAR=request.form['action6']
             return redirect(url_for('analytics_results'))
-        else:
-            pass # unknown
     elif request.method == 'GET':
         return render_template('analytics.html')
-    
     return render_template('analytics.html')
 
 @app.route('/analysis_results')
 def analysis_results():
-    
-    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    """
+    This function is used to provide the results for
+    analysis between 2 companies in the form of a graph
+    """
+    with pyodbc.connect('DRIVER='+DRIVER+
+        ';SERVER=tcp:'+SERVER+';PORT=1433;DATABASE='+DATABASE+
+        ';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT * from "+analysis_drop1+" ORDER BY year")
+            cursor.execute("SELECT * from "+ANALYSIS_DROP1+" ORDER BY year")
             row = cursor.fetchone()
             year_row=[]
             car_mauf_row=[]
-            
             while row:
                 year_row.append((row[0]))
                 car_mauf_row.append((row[2]))
                 row = cursor.fetchone()
-            data={"Year":year_row,"Cars_Manufactured "+analysis_drop1+" (in millions)":car_mauf_row}
+            data={"Year":year_row,"Cars_Manufactured "+ANALYSIS_DROP1+" (in millions)":car_mauf_row}
             df=pd.DataFrame(data)
-            
-
-    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+
+            ';PORT=1433;DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT * from "+analysis_drop2+" ORDER BY year")
+            cursor.execute("SELECT * from "+ANALYSIS_DROP2+" ORDER BY year")
             row = cursor.fetchone()
             year_row=[]
             car_mauf_row=[]
-            
             while row:
                 year_row.append((row[0]))
                 car_mauf_row.append((row[2]))
                 row = cursor.fetchone()
-            data1={"Year":year_row,"Cars_Manufactured "+analysis_drop2+" (in millions)":car_mauf_row}
+            data1={"Year":year_row,"Cars_Manufactured "+ANALYSIS_DROP2+
+            " (in millions)":car_mauf_row}
             df1=pd.DataFrame(data1)
-            
-    df['Cars_Manufactured '+analysis_drop2+' (in millions)']=df1['Cars_Manufactured '+analysis_drop2+' (in millions)']
-    
-    # x=df['Year']
-    # y=df.columns[1:3]
-    # fig1 = px.line(df, x="Year",y=df.columns[1:3])
-    # # a=fig1.show()
-    # # fig.show()
-    # # return redirect(url_for('analysis'))
-    # with open('graph.html', 'w') as f:
-    #     f.write(fig1.to_html(full_html=True, include_plotlyjs='cdn'))
-    
-    # return render_template('graph.html',labels=x ,values=y) 
-
-    # offline.plot(fig1, filename='graph.html')
-    # return render_template('graph.html')
-
-    # line_chart = pygal.Line()
-    # line_chart._title = 'Browser usage evolution (in %)'
-    # line_chart.x_labels = map(str, range(2002, 2013))
-    # line_chart.add = ('abc',[1,2,3,4])
-    # line_chart.add = ('def',[2,3,4,5])
-    # return line_chart.render_response()
+    df['Cars_Manufactured '+ANALYSIS_DROP2+' (in millions)']= \
+    df1['Cars_Manufactured '+ANALYSIS_DROP2+' (in millions)']
     custom_style = Style(
         background='#FFE5B4',
     )
-    line_chart = pygal.Line(x_title="Year",y_title="Cars_Manufactured(in millions)",legend_at_bottom=True,style=custom_style)
-    line_chart.title =str(analysis_drop1).capitalize() +' Vs '+str(analysis_drop2).capitalize()
+    line_chart = pygal.Line(x_title="Year",y_title="Cars_Manufactured(in millions)" \
+        ,legend_at_bottom=True,style=custom_style)
+    line_chart.title =str(ANALYSIS_DROP1).capitalize() +' Vs '+str(ANALYSIS_DROP2).capitalize()
     line_chart.x_labels = df['Year']
-    # line_chart._legend
-    # [line_chart.add(df[x],df[x].values) for x in df.columns[1:3]]
     for x in df.columns[1:3]:
         line_chart.add(x,df[x].values)
-    # return line_chart.render()
     return line_chart.render_response()
+
 @app.route('/analytics_results',methods=['GET', 'POST'])
 def analytics_results():
-    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    """
+    This function is used to provide analytics results based on the company and the year provided
+    """
+    with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+
+        ';PORT=1433;DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT * from "+analytics_drop+" ORDER BY year")
+            cursor.execute("SELECT * from "+ANALYTICS_DROP+" ORDER BY year")
             row = cursor.fetchone()
             year_row=[]
             car_mauf_row=[]
-            
             while row:
                 year_row.append((row[0]))
                 car_mauf_row.append((row[2]))
                 row = cursor.fetchone()
-            data={"Year":year_row,"Cars_Manufactured "+analytics_drop+" (in millions)":car_mauf_row}
-            df=pd.DataFrame(data)
-            X=df.iloc[:,:1].values
-            y=df.iloc[:,-1].values
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+            data={"Year":year_row,"Cars_Manufactured "+ANALYTICS_DROP+" (in millions)":car_mauf_row}
+            df_val=pd.DataFrame(data)
+            x_val=df_val.iloc[:,:1].values
+            y_val=df_val.iloc[:,-1].values
+            x_train, x_test, y_train, y_test = \
+                train_test_split(x_val,y_val, test_size=0.2, random_state=0)
             regressor = LinearRegression()
-            regressor.fit(X_train, y_train)
-            predicted_val=str(round((float(regressor.predict([[analytics_year]]))),2))
-            
-
-            return render_template('final_result.html',first_header=predicted_val,p1=analytics_drop,p2=analytics_year)
-
+            regressor.fit(x_train, y_train)
+            x_test=y_test+0+x_test
+            predicted_val=str(round((float(regressor.predict([[ANALYTICS_YEAR]]))),2))
+            return render_template('final_result.html' \
+                ,first_header=predicted_val,p1=ANALYTICS_DROP,p2=ANALYTICS_YEAR)
 if __name__ == '__main__':
     app.run(debug=True)
-
-
